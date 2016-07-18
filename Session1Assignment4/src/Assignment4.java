5503c87e890c38a7aa870f6587d1e6762c4ebf06
bharathscaphy <bhrthkumar75@gmail.com>
2016-07-19 03:17:43 +0530

    Initial GitHub Commit
    Initial GitHub Commit

---
+++ Session1Assignment4/src/Assignment4.java
@@ -0,0 +1,16 @@
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5,y=10;
+		int a = x+y*2;
+		int b = x-y+2;
+		int c = (x+y)*2;
+		int d = y%x;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
